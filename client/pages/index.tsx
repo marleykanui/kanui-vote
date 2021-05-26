f5b34763a93fe9b858af8d7b9d13aa0b302f14fb
Kanui <marley.kanui@gmail.com>
2021-05-26 12:02:26 -0500

    kanui-vote - global commit 05 - connected to web3 instance

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,34 @@
+// Web3
+import Web3 from 'web3';
+
 // React Types
 import { FC } from 'react';
 
-const KanuiVote: FC = () => <div>kanui vote - dapp init</div>;
+const KanuiVote: FC = () => {
+  const loadWeb3 = async () => {
+    //  @ts-ignore
+    if (window.ethereum) {
+      //  @ts-ignore
+      window.web3 = new Web3(window.ethereum);
+      //  @ts-ignore
+      await window.ethereum.enable();
+      //  @ts-ignore
+    } else if (window.web3) {
+      //  @ts-ignore
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert(
+        'Non-Ethereum browser detected. You should consider MetaMask'
+      );
+    }
+    // @ts-ignore
+    console.log(window.ethereum.selectedAddress);
+  };
+  return (
+    <div>
+      <button onClick={loadWeb3}> Connect to Metamask </button>
+    </div>
+  );
+};
 
 export default KanuiVote;
