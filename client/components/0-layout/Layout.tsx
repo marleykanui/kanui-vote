dd61a7f4aeb927a21669d0521a9f19325d5f03b3
Kanui <marley.kanui@gmail.com>
2021-05-27 10:59:04 -0500

    kanui-vote - global commit 10 - Set loading account data message

--- client/components/0-layout/Layout.tsx
+++ client/components/0-layout/Layout.tsx
@@ -1,5 +1,5 @@
 // React
-import { useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 
 // Components
 import Navbar from '@/components/0-layout/Navbar';
@@ -25,6 +25,8 @@ interface LayoutProps {
   children: ReactNode;
 }
 const Layout: FC<LayoutProps> = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+
   const {
     CurrentAccountAddress: { currentAccountAddress },
   } = useSelector<AppState, AppState>((state) => state);
@@ -32,6 +34,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   const dispatch = useDispatch();
 
   const loadBlockchainData = async () => {
+    setLoading(true);
     // @ts-ignore
     // Get Web3 Instance
     const web3 = window.web3;
@@ -51,6 +54,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
         ElectionAbi.abi,
         networkData.address
       );
+      setLoading(false);
     } else {
       window.alert('The Smart Contract is not deployed to the current network');
     }
@@ -64,11 +68,19 @@ const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <Fragment>
       <Navbar currentAccountAddress={currentAccountAddress} />
-      <div className="fixed inset-0 h-full max-w-full">
-        <div className="flex justify-center items-center container mx-auto h-full">
-          {children}
+      {loading ? (
+        <div className="fixed inset-0 h-full max-w-full">
+          <div className="flex justify-center items-center container mx-auto h-full">
+            <div>please wait while we load account data</div>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="fixed inset-0 h-full max-w-full">
+          <div className="flex justify-center items-center container mx-auto h-full">
+            {children}
+          </div>
+        </div>
+      )}
     </Fragment>
   );
 };
